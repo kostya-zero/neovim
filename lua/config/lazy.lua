e4d781d47b087e1dedb28794c85c4cf15b5d3ab7
Konstantin Zhigaylo <zero@kostyazero.com>
2026-02-13 09:10:18 +0300

    push latest config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -29,6 +29,7 @@ require("lazy").setup({
         { import = "lazyvim.plugins.extras.lang.tailwind" },
         { import = "lazyvim.plugins.extras.lang.sql" },
         { import = "lazyvim.plugins.extras.lang.go" },
+        { import = "lazyvim.plugins.extras.lang.python" },
         { import = "lazyvim.plugins.extras.lang.markdown" },
         { import = "lazyvim.plugins.extras.editor.outline" },
         { import = "lazyvim.plugins.extras.editor.neo-tree" },
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,6 +5,7 @@
 -- LazyVim specific options
 -- vim.g.lazyvim_blink_main = true
 vim.g.lazyvim_picker = "telescope"
+vim.g.snacks_animate = false
 
 -- Enable title
 vim.opt.title = true
@@ -34,3 +35,13 @@ vim.opt.mouse = ""
 
 -- Ignore paths
 vim.opt.wildignore:append({ "*/node_modules/*", "*/target/*" })
+
+-- Neovide
+vim.opt.guifont = "IosevkaTerm Nerd Font Mono:h16"
+vim.g.neovide_position_animation_length = 0
+vim.g.neovide_cursor_animation_length = 0.00
+vim.g.neovide_cursor_trail_size = 0
+vim.g.neovide_cursor_animate_in_insert_mode = false
+vim.g.neovide_cursor_animate_command_line = false
+vim.g.neovide_scroll_animation_far_lines = 0
+vim.g.neovide_scroll_animation_length = 0.00
